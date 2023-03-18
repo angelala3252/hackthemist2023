558bf2efb79d8dd8a566ab9436d1e9f61143bb54
Diian-r <dianrong2012@gmail.com>
2023-03-18 17:46:48 -0400

    Implemented the give_sentiment function

--- data2emotions.py
+++ data2emotions.py
@@ -2,12 +2,26 @@
 """
 
 import text2emotion
+from nltk.sentiment import SentimentIntensityAnalyzer
 
 
-def sadness(text: str) -> int:
+def give_sentiment(text: str) -> int:
     """
-    take the Sad decmal associated with the text
-    multiply it by 255
+    Return a float that represents the sentiment of the given text.
+    """
+    sia = SentimentIntensityAnalyzer()
+    sentiment = sia.polarity_scores(text)
+    result = sentiment['neg']
+
+    # convert for hex
+    result = result * 255
+    return round(result)
+
+
+def give_sadness(text: str) -> int:
+    """
+    Take the Sad decimal associated with the text and
+    multiply it by 255.
 
     """
     result = text2emotion.get_emotion(text)
@@ -16,3 +30,12 @@ def sadness(text: str) -> int:
     sad = sad * 255
     sad = round(sad)
     return sad
+
+
+def emotions_to_colour(emotions: dict) -> tuple:
+    """
+    Convert the emotions in emotions into a tuple of an RGB colour,
+    according to the following scales:
+
+
+    """
