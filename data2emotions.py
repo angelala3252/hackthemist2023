c257cff3d1a668ac51fa1a9dc169ac34f7ef0b74
angelala3252 <88949118+angelala3252@users.noreply.github.com>
2023-03-18 17:43:02 -0400

    fixed sadness function to return an int

--- data2emotions.py
+++ data2emotions.py
@@ -4,24 +4,7 @@
 import text2emotion
 
 
-def give_emotions(text: str) -> tuple:
-    """
-    Given a chunk of text, return a dictionary containing the scores of each emotion from {'Angry',
-    'Fear','Happy','Sad','Surprise'}
-    """
-    result = text2emotion.get_emotion(text)
-    return result
-
-
-def emotions_to_colour(emotions: dict) -> tuple:
-    """
-    Convert the emotions in emotions into a tuple of an RGB colour,
-    according to the following scales:
-
-    
-    """
-
-def sadness(text: str) -> float:
+def sadness(text: str) -> int:
     """
     take the Sad decmal associated with the text
     multiply it by 255
@@ -29,7 +12,7 @@ def sadness(text: str) -> float:
     """
     result = text2emotion.get_emotion(text)
     sad = result['Sad']
-    #get the correct amount
-    sad = sad*255
-    sad.round()
+    # get the correct amount
+    sad = sad * 255
+    sad = round(sad)
     return sad
