71e4e5e64b2fb001f5633e982345a0a3cbfb72f9
angelala3252 <88949118+angelala3252@users.noreply.github.com>
2023-03-18 16:49:28 -0400

    wrote skeleton of text to colour functions

--- data2emotions.py
+++ data2emotions.py
@@ -2,3 +2,20 @@
 """
 
 import text2emotion
+
+
+def give_emotions(text: str) -> tuple:
+    """
+    Given a chunk of text, return a dictionary containing the scores of each emotion from {'Angry',
+    'Fear','Happy','Sad','Surprise'}
+    """
+    result = text2emotion.get_emotion(text)
+    return result
+
+
+def emotions_to_colour(emotions: dict) -> tuple:
+    """
+    Convert the emotions in emotions into a tuple of an RGB colour,
+    according to the following scales:
+
+    """
